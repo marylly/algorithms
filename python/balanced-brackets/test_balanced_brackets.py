f800ac1b4b69f0887be3908601a429315836a23e
Marylly Araújo Silva <mymarylly@gmail.com>
2020-09-20 18:27:34 -0300

    Improving balanced brackets class test case

--- python/balanced-brackets/test_balanced_brackets.py
+++ python/balanced-brackets/test_balanced_brackets.py
@@ -2,43 +2,35 @@ import unittest
 from balanced_brackets.BalancedBrackets import BalancedBrackets
 
 class Test_Balanced_Brackets(unittest.TestCase):
-    START = None
-    graph = None
+    expression = None
+    bb = None
 
     def setUp(self):
+        self.expression = "{{[[(())]]}}"
+        self.bb = BalancedBrackets(self.expression)
         return super().setUp()
 
     def test_expression_length(self):
-        expression = "{{[[(())]]}}"
-        bb = BalancedBrackets(expression)
-        self.assertEqual(bb.get_lenght(),12)
+        self.assertEqual(self.bb.get_lenght(),12)
 
     def test_qtd_brackets_pairs(self):
-        expression = "{{[[(())]]}}"
-        bb = BalancedBrackets(expression)
-        self.assertEqual(bb.get_qtd_brackets_pairs(),6)
+        self.assertEqual(self.bb.get_qtd_brackets_pairs(),6)
 
     def test_get_brackets(self):
-        expression = "{{[[(())]]}}"
-        bb = BalancedBrackets(expression)
-        self.assertEqual(bb.get_brackets(),['{','{','[','[','(','(',')',')',']',']','}','}'])
+        self.assertEqual(self.bb.get_brackets(),['{','{','[','[','(','(',')',')',']',']','}','}'])
 
     def test_get_opened_brackets(self):
-        expression = "{{[[(())]]}}"
-        bb = BalancedBrackets(expression)
-        self.assertEqual(bb.get_opened_brackets(),['{','{','[','[','(','('])
+        self.assertEqual(self.bb.get_opened_brackets(),['{','{','[','[','(','('])
 
     def test_get_closing_brackets(self):
-        expression = "{{[[(())]]}}"
-        bb = BalancedBrackets(expression)
-        self.assertEqual(bb.get_closing_brackets(),[')',')',']',']','}','}'])
+        self.assertEqual(self.bb.get_closing_brackets(),[')',')',']',']','}','}'])
 
     def test_is_balanced_brackets(self):
-        expression = "{{[[(())]]}}"
-        bb = BalancedBrackets(expression)
-        self.assertEqual(bb.is_balanced_brackets(),'YES')
+        self.assertEqual(self.bb.is_balanced_brackets(),'YES')
 
     def tearDown(self):
+        self.expression = None
+        self.bb = None
         return super().tearDown()
 
 if __name__ == '__main__':
